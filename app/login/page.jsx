eab30ce42078b726491cb20b0a9f3ffc37584428
nohsoobin <nohsoobin18@naver.com>
2023-06-16 16:18:11 +0900

    0616

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-xl mb-4">로그인 페이지</h2>
-      {/* 로그인 페이지 내용 추가 */}
+    <div>
+      <div class="container text-center">
+        <form class="form-signin mt-5">
+          <h1 class="h3 mb-3 font-weight-normal">Log In 🔐</h1>
+          <br />
+          <br />
+          <label for="inputid" class="sr-only">
+            ID
+          </label>
+          <input
+            type="text"
+            id="inputid"
+            class="form-control"
+            placeholder="아이디"
+          />
+          <br />
+          <label for="inputPassword" class="sr-only">
+            Password
+          </label>
+          <input
+            type="password"
+            id="inputPassword"
+            class="form-control"
+            placeholder="비밀번호"
+          />
+          <div class="checkbox mb-3">
+            <label class="float-left">
+              <input type="checkbox" value="remember-me" />
+              아이디 기억하기
+            </label>
+          </div>
+          <button
+            class="btn btn-lg btn-danger btn-block click"
+            id="login"
+            type="submit"
+          >
+            log in
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>회원가입 페이지</div>
-  )
-}
+    <div>
+      <div className="container text-center">
+        <form className="form-signin">
+          <h1 className="h3 mt-5 font-weight-normal">Sign Up✍️</h1>
+          <div className="mt-5">
+            <div className="form-group">
+              <label htmlFor="lastname">성 :</label>
+              <input
+                type="text"
+                id="lastname"
+                placeholder="성 (last name)"
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="firstname">이름 :</label>
+              <input
+                type="text"
+                id="firstname"
+                placeholder="이름 (first name)"
+                className="form-control"
+                required
+              />
+            </div>
 
-export default page
+            <div className="form-group">
+              <label htmlFor="nickname">닉네임 :</label>
+              <div className="input-group">
+                <input
+                  type="text"
+                  id="nickname"
+                  placeholder="닉네임(띄어쓰기 없이 2자 이상)"
+                  className="form-control nickname"
+                  required
+                />
+                <div className="input-group-append">
+                  <button className="btn btn-outline-secondary" type="button">
+                    중복확인
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="call">전화번호 :</label>
+              <div className="input-group">
+                <input
+                  type="num"
+                  id="call"
+                  placeholder="-없이 전화번호를 입력해주세요"
+                  className="form-control num"
+                  required
+                />
+                <div className="input-group-append">
+                  <button className="btn btn-outline-secondary" type="button">
+                    인증번호 전송
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="id">사용자 이름 :</label>
+              <input
+                type="text"
+                id="id"
+                placeholder="아이디를 입력해주세요"
+                className="form-control"
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="passwd">비밀번호 :</label>
+              <input
+                type="password"
+                id="passwd"
+                placeholder="비밀번호를 입력해주세요"
+                className="form-control"
+                required
+              />
+            </div>
+
+            <button className="btn btn-lg btn-danger btn-block" type="submit">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
